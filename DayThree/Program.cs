10d55216be6c7fc150049ca72bb1e60a3dc0a6e9
Casper <casperb.andresen@proton.me>
2023-01-25 19:08:56 +0100

    day three solved

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,29 +1,72 @@
-﻿PartOne();
+﻿using System.Linq;
 
-static void PartOne(){
+PartOne();
+
+PartTwo();
+
+static void PartOne()
+{
     string rucksacks = File.ReadAllText("DayThree/input.txt").TrimEnd();
 
-            var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
 
-            string[] individualRucksacks = rucksacks.Split("\n");
+    string[] individualRucksacks = rucksacks.Split("\n");
 
-            int priority = 0;
+    int priority = 0;
 
-            foreach (string rucksack in individualRucksacks)
+    foreach (string rucksack in individualRucksacks)
+    {
+        char[] chars = rucksack.ToCharArray();
+        char[] firstRucksack = chars.Take(chars.Length / 2).ToArray();
+        char[] secondRucksack = chars.Skip(chars.Length / 2).ToArray();
+
+        foreach (char c in secondRucksack)
+        {
+            if (firstRucksack.Contains(c))
             {
-                char[] chars = rucksack.ToCharArray();
-                char[] firstRucksack = chars.Take(chars.Length / 2).ToArray();
-                char[] secondRucksack = chars.Skip(chars.Length / 2).ToArray();
-
-                foreach (char c in secondRucksack)
-                {
-                    if (firstRucksack.Contains(c))
-                    {
-                        priority += Array.IndexOf(characters, c) + 1;
-                        break;
-                    }
-                }
+                priority += Array.IndexOf(characters, c) + 1;
+                break;
             }
+        }
+    }
 
-            Console.WriteLine(priority);
+    Console.WriteLine(priority);
+}
+
+static void PartTwo()
+{
+
+    string rucksacks = File.ReadAllText("DayThree/input.txt").TrimEnd();
+
+    var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+    string[] individualRucksacks = rucksacks.Split("\n");
+
+    int priority = 0;
+
+    for (var i = 2; i < individualRucksacks.Length; i += 3)
+    {
+        string rucksack1 = individualRucksacks[i - 2];
+        string rucksack2 = individualRucksacks[i - 1];
+        string rucksack3 = individualRucksacks[i];
+
+        HashSet<char> chars1 = new HashSet<char>(rucksack1);
+        HashSet<char> chars2 = new HashSet<char>(rucksack2);
+        HashSet<char> chars3 = new HashSet<char>(rucksack3);
+
+        string v = rucksack1 + rucksack2 + rucksack3;
+
+        foreach (char c in v) {
+            if( chars1.Contains(c) && chars2.Contains(c) && chars3.Contains(c))
+            {
+                priority += Array.IndexOf(characters, c) + 1;
+                break;
+            }
+        }
+
+
+
+    }
+
+    Console.WriteLine(priority);
 }
