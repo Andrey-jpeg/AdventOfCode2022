9e5110e885828e33f22e15775fdd1d76e6daef17
Casper <casperb.andresen@proton.me>
2023-01-29 13:48:14 +0100

    solution for day 4

---
+++ DayFour/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+PartOne();
+PartTwo();
+
+static void PartOne()
+{
+    string sectionAssignments = File.ReadAllText("DayFour/input.txt").TrimEnd();
+
+    string[] pairs = sectionAssignments.Split("\n");
+    
+    int count = 0;
+    foreach (var pair in pairs)
+    {
+        var numbers = Regex.Matches(pair, @"\d+").Select(m => int.Parse(m.Value)).ToList();
+
+        if((numbers[0] <= numbers[2] && numbers[1] >= numbers[3]) || (numbers[2] <= numbers[0] && numbers[3] >= numbers[1])) {
+            count += 1;
+        }   
+    }
+
+    System.Console.WriteLine(count);
+}
+
+static void PartTwo()
+{
+    string sectionAssignments = File.ReadAllText("DayFour/input.txt").TrimEnd();
+
+    string[] pairs = sectionAssignments.Split("\n");
+    
+    int count = 0;
+    foreach (var pair in pairs)
+    {
+        var numbers = Regex.Matches(pair, @"\d+").Select(m => int.Parse(m.Value)).ToList();
+
+        if( numbers[0] <= numbers[3] && numbers[2] <= numbers[1])
+        {
+            count += 1;
+        }   
+    }
+
+    System.Console.WriteLine(count);
+}
