9d5c78979bc67f2108b6f46c7942202c5e426b78
Casper <casperb.andresen@proton.me>
2023-01-31 12:58:23 +0100

    DaySix Solve

---
+++ DaySix/Program.cs
@@ -0,0 +1,34 @@
+﻿PartOne();
+PartTwo();
+
+static void PartOne()
+{
+    string datastream = File.ReadAllText("input.txt").TrimEnd();
+    var chars = datastream.ToArray();
+
+    for (var i = 0; i < chars.Length; i++)
+    {
+        var subChars = chars.ToList().GetRange(i, 4);
+        if(subChars.Distinct().Count() == subChars.Count())
+        {
+            Console.WriteLine(i + subChars.Count());
+            break;
+        }
+    }
+}
+
+static void PartTwo()
+{
+    string datastream = File.ReadAllText("input.txt").TrimEnd();
+    var chars = datastream.ToArray();
+
+    for (var i = 0; i < chars.Length; i++)
+    {
+        var subChars = chars.ToList().GetRange(i, 14);
+        if(subChars.Distinct().Count() == subChars.Count())
+        {
+            Console.WriteLine(i + subChars.Count());
+            break;
+        }
+    }
+}
